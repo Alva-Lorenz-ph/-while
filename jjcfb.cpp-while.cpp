e1a2c23f971192f39af895ebee322623fa3b9bf9
Alva-Lorenz-ph <2135774961@qq.com>
2025-10-25 15:52:48 +0800

    Add files via upload

---
+++ jjcfb.cpp-while.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	a=1
+	while( a <= 9;)
+	{
+		int b;
+		for (b = 1; b <= a;b= b + 1)
+		{
+			cout << b << "*" << a << "=" << a * b << " ";
+		}
+		a = a + 1;
+		cout << endl;
+
+	}
+	return 0;
+}
